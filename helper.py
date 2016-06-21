c8adfbe66989fb7178314431d8be8689ff29f588
xmduhan <xmduhan@gmail.com>
2016-06-21 10:10:39 +0800

    ...

---
+++ helper.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from pandas import HDFStore
+
+
+def page_view_stat(page_view_data):
+    """ 点击量清单统计 """
+
+    # 统计总体情况
+    g = page_view_data.groupby(['dev_id', 'post_id'])
+    d = g[['stat_view', 'stat_click']].sum()
+    d['days'] = g['stat_date'].nunique()
+
+    # 统计type=1
+    c1 = page_view_data['type'] == 1
+    g1 = page_view_data[c1].groupby(['dev_id', 'post_id'])
+    d1 = g1[['stat_view', 'stat_click']].sum()
+    d1['days'] = g1['stat_date'].nunique()
+
+    # 统计type=2
+    c2 = page_view_data['type'] == 2
+    g2 = page_view_data[c2].groupby(['dev_id', 'post_id'])
+    d2 = g2[['stat_view', 'stat_click']].sum()
+    d2['days'] = g2['stat_date'].nunique()
+
+    # 合并结果集
+    d = d.join(d1, rsuffix=('_1'))
+    d = d.join(d2, rsuffix=('_2'))
+    d = d.fillna(0)
+    d = d.reset_index()
+
+    # 返回结果
+    return d
+
+
+def removeDataFrame(dfName):
+    dbName = 'db.h5'
+    s = HDFStore(dbName)
+    if s.get_node(dfName):
+        s.remove(dfName)
+    s.close()
